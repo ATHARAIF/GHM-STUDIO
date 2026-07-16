b8f1611d9ca90c9462be41e4749b1b9dab96eee4
HaidarIhsan404 <haidarihsan556@gmail.com>
2026-07-16 14:34:31 +0700

    inisiasi

---
+++ scripts/tiles/ground.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+func _ready() -> void:
+	PlacementManager.register_camera($Camera3D)
+	PlacementManager.register_ground_tiles(_get_ground_tiles())
+
+func _get_ground_tiles() -> Array[GroundTile]:
+	var tiles: Array[GroundTile] = []
+	_collect_ground_tiles(self, tiles)
+	return tiles
+
+func _collect_ground_tiles(node: Node, tiles: Array[GroundTile]) -> void:
+	for child in node.get_children():
+		if child is GroundTile:
+			tiles.append(child)
+		_collect_ground_tiles(child, tiles)   # tetep turun walau child-nya sendiri GroundTile,
+											   # jaga-jaga kalau ada nested group lagi di dalemnya
